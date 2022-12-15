041745de327554bda34471cfd82c114c84e5e8d0
Elena Tarasova <lena.daschkina@yandex.ru>
2022-12-15 14:11:27 +0300

    метод возвращающий массив на основании заданных условий длина строки меньше или равна трем

--- finalhomework/Program.cs
+++ finalhomework/Program.cs
@@ -7,3 +7,33 @@
 string[] array = AskArray();
 string[] result = FindLessThan(array, 3);
 Console.WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+//метод возвращающий массив на основании заданных условий длина строки меньше или равна трем
+string[] FindLessThan(string[] input, int n) 
+{
+    string[] output = new string[CountLessThan(input, n)];
+
+    for(int i = 0, j = 0; i < input.Length; i++) 
+    {
+        if(input[i].Length <= n) 
+        {
+            output[j] = input[i];
+            j++;
+        }
+    }
+
+    return output;
+}
+int CountLessThan(string[] input, int n) 
+{
+    int count = 0;
+
+    for(int i = 0; i < input.Length; i++) 
+    {
+        if(input[i].Length <= n) 
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
